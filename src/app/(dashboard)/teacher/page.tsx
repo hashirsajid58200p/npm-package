5799d1607ed195fbc19d24e14603b9496c56c6ef
hashirsajid58200p <hs58200p@gmail.com>
2026-07-16 21:25:18 +0500

    Add table and calendar enhancements

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,23 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+
 const TeacherPage = () => {
   return (
-    <div className=''>TeacherPage</div>
-  )
-}
+    <div className="flex-1 p-4 flex flex-col gap-4 xl:flex-row">
+      {/* Left */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule</h1>
+          <BigCalendar />
+        </div>
+      </div>
+      {/* Right */}
 
-export default TeacherPage
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        <Announcements />
+      </div>
+    </div>
+  );
+};
+
+export default TeacherPage;
